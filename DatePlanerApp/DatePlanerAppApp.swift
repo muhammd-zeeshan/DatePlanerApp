cb8b1f941bc60cb44c00f8ddc9152362c9a4a158
muhmmad-zeeshan <rafaq657@gmail.com>
2024-11-28 01:03:42 +0500

    Initial Commit

---
+++ DatePlanerApp/DatePlanerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlanerAppApp.swift
+//  DatePlanerApp
+//
+//  Created by Muhammad Zeeshan on 28/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct DatePlanerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
